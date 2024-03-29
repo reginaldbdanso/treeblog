8b1e0bb9de2a3a8e954cd27eae876703d7ff62f9
reginaldbdanso <reginaldbdanso@gmail.com>
2024-03-29 14:54:21 +0000

    Deleted a comment

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,4 @@ mongoose.connect(process.env.MONGO_URI)
     })
     .catch((err) => {console.log(err)})
 
-// app.listen(process.env.PORT, () => {
-//     console.log('connected to db and listening on port', process.env.PORT)
-// })
+
