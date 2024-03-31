6a61f7847957fe5ebff2cd3125b831a1d93bbe28
reginaldbdanso <reginaldbdanso@gmail.com>
2024-03-31 09:25:18 +0000

    Key Changes: -Update styles and add authentication context provider so that user state can be accessed globally in the app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { MyBlogContextProvider } from './context/BlogContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyBlogContextProvider>
-      <App />
-    </MyBlogContextProvider>
+    <AuthContextProvider>
+      <MyBlogContextProvider>
+        <App />
+      </MyBlogContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
