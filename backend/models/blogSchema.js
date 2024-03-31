f298761c95a092a1f9fbadd4987434d7b153b0e5
reginaldbdanso <reginaldbdanso@gmail.com>
2024-03-31 09:20:56 +0000

    Key Changes: -Add user_id field to blogSchema so that each blog can have the id of the user that creaeted it

--- backend/models/blogSchema.js
+++ backend/models/blogSchema.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 const blogSchema = new Schema({
     title: { type: String, required: true },
     body: { type: String, required: true },
-    author: { type: String, required: true}
+    author: { type: String, required: true},
+    user_id: { type: String, required: true}
 }, {timestamps: true});
 
 module.exports = mongoose.model('Blog', blogSchema);
