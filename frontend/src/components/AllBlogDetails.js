597758c4f74f743945a5f2e764a51f1b160d1544
reginaldbdanso <reginaldbdanso@gmail.com>
2024-03-31 16:30:39 +0000

    Removed unused code in AllBlogDetails component

--- frontend/src/components/AllBlogDetails.js
+++ frontend/src/components/AllBlogDetails.js
@@ -1,29 +1,7 @@
-// import useBlogsContext from "../hooks/useBlogContext";
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-// import { useAuthContext } from '../hooks/useAuthContext';
-
 
 const AllBlogDetails = ({blog}) => {
-    // const {dispatch} = useBlogsContext();
-    // const {user} = useAuthContext();
 
-    // const handleClick = async () => {
-    //     if (!user) {
-    //         return
-    //     }
-        
-    //     const response = await fetch(process.env.REACT_APP_API_URL + '/' + blog._id, {
-    //         method: 'DELETE',
-    //         headers: {
-    //                "Authorization": `Bearer ${user.token}`
-    //         }
-    //     })
-    //     // const json = await response.json();
-
-    //     if (response.ok) {
-    //         dispatch({type: 'DELETE_BLOG', payload: blog._id});
-    //     }
-    // }
 
     return (
         <div className="allblog-details">
