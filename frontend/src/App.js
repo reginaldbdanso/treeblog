cfa3d57bc8a7b5f83b5bdd099a164d0155dfbf54
reginaldbdanso <reginaldbdanso@gmail.com>
2024-03-31 09:26:27 +0000

    Key Changes: -Update routes to include guards and added authentication checks to determine whether to redirect or not

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,36 @@
 // import logo from './logo.svg';
 // import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+
+
 import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import About from './pages/About';
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Footer from './components/Footer';
+
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" exact element={<Home/>} />
+            <Route path="/" exact element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/blogs" exact element={user ? <Blogs /> : <Navigate to="/login" />} />
+            <Route path="/about" exact element={user ? <About /> : <Navigate to="/login" />} />
+            <Route path="/login" exact element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path="/signup" exact element={!user ? <Signup /> : <Navigate to="/" />} />
           </Routes>
         </div>
-      </BrowserRouter> 
-      
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
