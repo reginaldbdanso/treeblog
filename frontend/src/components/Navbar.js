e95d4b8416562a34c4e5ee78dd472925e11a0a1b
reginaldbdanso <reginaldbdanso@gmail.com>
2024-03-31 09:31:57 +0000

    Key Changes: -Added logout functionality and conditional rendering of navigation links

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+    const { logout } = useLogout()
+    const navigate = useNavigate()
+    const { user } = useAuthContext()
+
+    const handleLogout = () => {
+        logout();
+        navigate('/login');
+    }
+
     return (
         <header>
             <div className="container">
@@ -8,10 +20,22 @@ const Navbar = () => {
                     <h1>TreeBlog</h1>
                 </Link>
                 <div className="navlinks">
-                    <Link to='/'>Home</Link>
-                    <Link to='/blogs'>Blogs</Link>
-                    <Link to='/about'>About</Link>
-                    <Link to='/contact'>Contact</Link>
+                    <nav>
+                    {user && (<Link to='/'>Home</Link>)}
+                    {user && (<Link to='/blogs'>Blogs</Link>)}
+                    {user && (<Link to='/about'>About</Link>)}
+                    {!user && (
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </div>)}
+                    {user && (
+                    <div>
+                        <button onClick={handleLogout}>Logout</button>
+                        {/* <span>{user.email}</span> */}
+                    </div>)}
+                    </nav>
+                    {user && (<span>{user.email}</span>)}
                 </div>
             </div>
         </header>
