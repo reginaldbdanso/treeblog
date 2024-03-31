7db9ab2e34a26a33e14728a60eff57bb4898232d
reginaldbdanso <reginaldbdanso@gmail.com>
2024-03-31 09:31:38 +0000

    Key Changes: -Added Featured and Footer components with Terms and Conditions

---
+++ frontend/src/components/Terms.js
@@ -0,0 +1,17 @@
+const Terms = () => {
+    return (
+        <div className="terms">
+            <h1>Terms and Conditions</h1>
+            <p>These terms and conditions outline the rules and regulations for the use of TreeBlog's Website.</p>
+            <p>
+                1. By accessing and using this website, you agree to be bound by these terms and conditions, which govern your use of this website. If you disagree with any part of these terms and conditions, please do not use our website.
+            </p>
+            <p>
+                2. The content of this website is for your general information and use only. It is subject to change without notice. Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness, or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors, and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.
+            </p>
+
+        </div>
+    );
+}
+
+export default Terms;
